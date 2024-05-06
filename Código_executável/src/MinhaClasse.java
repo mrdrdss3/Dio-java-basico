7369be3b49928ad94c1733d753f34a956c6d3a0f
mrdrdss3 <mrdrdss@hotmail.com>
2024-05-06 19:18:21 -0300

    Código aula 3

--- Código_executável/src/MinhaClasse.java
+++ Código_executável/src/MinhaClasse.java
@@ -1,5 +1,18 @@
 public class MinhaClasse{
+
     public static void main (String [] args){
-        System.out.print("Olá, seja bem-vindo!");
+        
+        String primeiroNome = "Maria";
+        String segundoNome = "Eduarda";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.print(nomeCompleto);
+
     }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
